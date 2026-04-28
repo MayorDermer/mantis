73a4934620884a1b3bbfbc23b74ac5bd3fe47e88
Mayor Dermer <123976489+MayorDermer@users.noreply.github.com>
2026-04-28 16:03:42 +0300

    mid work commit
    Signed-off-by: Mayor Dermer <123976489+MayorDermer@users.noreply.github.com>

--- src/mantis.cpp
+++ src/mantis.cpp
@@ -3,31 +3,51 @@
 #include <iostream>
 #include <mantis/mantis.h>
 
+static constexpr double BUFF_SIZE = 1e5;
+static constexpr double threshold = 1e-6;
+
+#define PASSES_THRESHOLD(a, b) (std::abs(a - b) < threshold)
+
 namespace po = boost::program_options;
 
-static std::string program_description = "Mantis Utils. Use with --help to see available options.";
+static std::string program_description = "Mantis Bin. Use --help/-h for options.";
 
 int main(int argc, char** argv) {
 
-    bool version;
+    /// params and flags
+    /// TODO: consider using a per channel struct for these when writing up multichannel maybe parse as a json and take
+    /// channel files? could add a mantis gen file option to create editable default json for users. perhaps a seperate
+    /// bin for this?
+    std::string args;
+    std::string filename;
+
+    size_t channel_num;
+    size_t reps;
+
+    double gain;
+    double rate;
+    double freq;
+    double mcr;
+
     bool find;
     bool tx_from_file;
     bool rx_to_file;
-    std::string args;
-    std::string filename;
-    size_t channel_num;
-    size_t reps;
+    bool version;
     bool repeat;
 
     po::options_description desc(program_description + "\nOptions");
     auto opts = desc.add_options();
+
+    // program modes
     opts("help,h", "produce help message");
     opts("find,f", po::bool_switch(&find)->default_value(false),
          "find connected devices. may be used with the 'args' option to find specific devices");
     opts("version,v", po::bool_switch(&version)->default_value(false), "print mantis version");
-    opts("args", po::value<std::string>(&args)->default_value(""), "device args str. Leave empty to find all");
     opts("tx_from_file,t", po::bool_switch(&tx_from_file)->default_value(false), "transmit from file");
     opts("rx_to_file,r", po::bool_switch(&rx_to_file)->default_value(false), "receive to file");
+
+    // args
+    opts("args,a", po::value<std::string>(&args)->default_value(""), "device args str. Leave empty to find all");
     opts("filename,F", po::value<std::string>(&filename)->default_value(""),
          "absolute path of file to transmit/receive from");
     opts("channel_num,c", po::value<size_t>(&channel_num)->default_value(0), "channel number for tx/rx. default 0");
@@ -36,6 +56,12 @@ int main(int argc, char** argv) {
     opts("repeat,R", po::bool_switch(&repeat)->default_value(false),
          "used with tx_from_file/t, repeat tx indefinetly. Alternatively, use --reps/-n to specify a number of "
          "repetitions");
+    opts("freq", po::value<double>(&freq)->default_value(100e6, "100e6"), "Freq [Hz], will default to 100MHz");
+    opts("lo", po::value<double>(), "lo offset freq [Hz] Will allow SDR lib to choose most fitting lo if not passed");
+    opts("rate", po::value<double>(&rate)->default_value(10e6, "10e6"), "Rate [Hz], will default to 10 MHz");
+    opts("gain", po::value<double>(&gain)->default_value(0.0, "0.0"), "Gain [dB], will default to 0 dB");
+    opts("master_clock_rate,mcr", po::value<double>(&mcr),
+         "master clock rate; can also be passed in args as master_clock_rate. This argument takes precedence");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -61,9 +87,9 @@ int main(int argc, char** argv) {
         }
     }
 
-    if (find) {
-        auto found_devices = d_manager.find(params);
+    auto found_devices = d_manager.find(params);
 
+    if (find) {
         if (found_devices.empty()) {
             std::cout << "No Devices Found\n";
             return EXIT_SUCCESS;
@@ -102,24 +128,74 @@ int main(int argc, char** argv) {
         mantis::utils::perror("Unknown sample formant");
         return EXIT_FAILURE;
     }
-    (void)sample_size; // TODO: this just for unused code warning
 
+    /// config lo
+    std::optional<double> lo = (vm.count("lo") ? std::optional<double>{vm["lo"].as<double>()} : std::nullopt);
+
+    /// alloc buffer
+    char* buff = (char*)malloc(BUFF_SIZE * sizeof(char));
+    if (!buff) {
+        mantis::utils::perror("Failed to malloc samples buffer");
+        return EXIT_FAILURE;
+    }
+
+    /// tx from file
     if (tx_from_file) {
+
         /// try to open file
         std::ifstream file(filename, std::ios::in | std::ios::binary);
         if (!file.is_open()) {
             mantis::utils::perror("Failed to open file: " + filename);
             return EXIT_FAILURE;
         }
-        auto [err, channel] = d_manager.get_tx_channel(params, channel_num);
+
+        /// acquire channel
+        auto [err, tx_channel] = d_manager.get_tx_channel(params, channel_num);
         if (!mantis::errors::succeeded(err)) {
-            mantis::utils::perror("Failed to Acquire Tx Channel: " + mantis::errors::mantis_errno(err));
-            return EXIT_FAILURE;
+            mantis::utils::perror("Failed to Acquire Channel: " + mantis::errors::mantis_errno(err));
         }
 
-        std::cout << "Transmitting from file...\n";
+        /// configure channel
+        double ac_freq = tx_channel->set_freq(freq, lo);
+        if (!PASSES_THRESHOLD(ac_freq, freq)) {
+            mantis::utils::pwarn("Couldn't set freq to: " + std::to_string(freq) +
+                                 ". Actual freq: " + std::to_string(ac_freq) + " Hz");
+        }
+
+        double ac_gain = tx_channel->set_gain(gain);
+        if (!PASSES_THRESHOLD(ac_gain, gain)) {
+            mantis::utils::pwarn("Couldn't set gain to: " + std::to_string(gain) +
+                                 ". Actual gain: " + std::to_string(ac_gain) + " dB");
+        }
+
+        double ac_rate = tx_channel->set_rate(rate);
+        if (!PASSES_THRESHOLD(ac_rate, rate)) {
+            mantis::utils::pwarn("Couldn't set rate to: " + std::to_string(rate) +
+                                 ". Actual rate: " + std::to_string(ac_rate) + " Hz");
+        }
+
+        /// metadata
+        mantis::go::mtx_metadata tx_md{.start_of_burst = true, .end_of_burst = false};
+
+        mantis::utils::pinfo("Beginning TX");
+        while (repeat || reps--) {
+            while (file.read(buff, BUFF_SIZE) || file.gcount() > 0) {
+                if (!tx_channel->is_valid()) [[unlikely]] {
+                    mantis::utils::perror("SDR healthcheck Failed, channel invalid");
+                    throw mantis::runtime_error("Invalid Channel");
+                }
+                tx_channel->send(buff, sample_size, BUFF_SIZE / sample_size, tx_md);
+            }
+            file.clear();
+            file.seekg(0, std::ios::beg);
+        }
+
+        free(buff);
+
+        mantis::utils::pinfo("Done");
     }
 
+    /// rx from file
     if (rx_to_file) {
         /// try opening file
         std::ofstream file(filename, std::ios::out | std::ios::binary);
@@ -127,15 +203,50 @@ int main(int argc, char** argv) {
             mantis::utils::perror("Failed to open file: " + filename);
             return EXIT_FAILURE;
         }
-        
-        auto [err, channel] = d_manager.get_rx_channel(params, channel_num);
+
+        /// acquire channel
+        auto [err, rx_channel] = d_manager.get_rx_channel(params, channel_num);
         if (!mantis::errors::succeeded(err)) {
             mantis::utils::perror("Failed to Acquire Rx Channel: " + mantis::errors::mantis_errno(err));
             return EXIT_FAILURE;
         }
 
+        /// config channel
+        double ac_freq = rx_channel->set_freq(freq, lo);
+        if (!PASSES_THRESHOLD(ac_freq, freq)) {
+            mantis::utils::pwarn("Couldn't set freq to: " + std::to_string(freq) +
+                                 ". Actual freq: " + std::to_string(ac_freq) + " Hz");
+        }
 
-        std::cout << "Receiving to file...\n";
+        double ac_gain = rx_channel->set_gain(gain);
+        if (!PASSES_THRESHOLD(ac_gain, gain)) {
+            mantis::utils::pwarn("Couldn't set gain to: " + std::to_string(gain) +
+                                 ". Actual gain: " + std::to_string(ac_gain) + " dB");
+        }
+
+        double ac_rate = rx_channel->set_rate(rate);
+        if (!PASSES_THRESHOLD(ac_rate, rate)) {
+            mantis::utils::pwarn("Couldn't set rate to: " + std::to_string(rate) +
+                                 ". Actual rate: " + std::to_string(ac_rate) + " Hz");
+        }
+
+        /// metadata
+        mantis::go::mrx_metadata rx_md{.start_of_burst = true};
+
+        mantis::utils::pinfo("Beginning Rx, will run until stopped");
+
+        while (true) {
+            if (!rx_channel->is_valid()) [[unlikely]] {
+                mantis::utils::perror("SDR healthcheck Failed, channel invalid. Shutting Down...");
+                return EXIT_FAILURE;
+            }
+
+            rx_channel->receive(buff, sample_size, BUFF_SIZE / sample_size, rx_md);
+            file.write(buff, BUFF_SIZE);
+        }
+
+        free(buff); // yes this does absolutely nothing but doesnt feel right without it
     }
+
     return EXIT_SUCCESS;
 }
