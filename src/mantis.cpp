9074a536b472471cdc8fc603318d53c408abf6c6
dermayor <123976489+dermayor@users.noreply.github.com>
2026-04-30 22:54:00 +0300

    missing return
    Signed-off-by: dermayor <123976489+dermayor@users.noreply.github.com>

--- src/mantis.cpp
+++ src/mantis.cpp
@@ -148,6 +148,7 @@ int main(int argc, char** argv) {
         auto [err, tx_channel] = d_manager.get_tx_channel(params, channel_num);
         if (!mantis::errors::succeeded(err)) {
             mantis::utils::perror("Failed to Acquire Channel: " + mantis::errors::mantis_errno(err));
+            return EXIT_FAILURE;
         }
 
         /// configure channel
