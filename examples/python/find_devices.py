f364591d8f0b100e672c4034c50fc65ec05e5075
Mayor Dermer <123976489+MayorDermer@users.noreply.github.com>
2026-05-04 20:51:06 +0300

    find done
    Signed-off-by: Mayor Dermer <123976489+MayorDermer@users.noreply.github.com>

--- examples/python/find_devices.py
+++ examples/python/find_devices.py
@@ -1,4 +1,5 @@
 import mantis
+from mantis import errors as me
 import argparse
 
 def main():
@@ -9,20 +10,22 @@ def main():
     d_manager = mantis.device_manager.get_instance()
 
     err, params = mantis.msdr_params.from_str(args.args)
-    print(err)
+    if err != me.error_code.SUCCESS:
+        print(me.mantis_errno(err))
+        return
 
     found_devices = d_manager.find(params)
 
-    # if(len(found_devices) == 0):
-    #     print("No devices found")
-    #     return
+    if(len(found_devices) == 0):
+        print("No devices found")
+        return
 
-    # for i in range(len(found_devices)):
-    #     print("=================================")
-    #     print(f"Mantis Device {i}")
-    #     print("=================================")
-    #     print(found_devices[i])
-    #     print("\n\n")
+    for i in range(len(found_devices)):
+        print("=================================")
+        print(f"Mantis Device {i}")
+        print("=================================")
+        print(found_devices[i])
+        print("\n\n")
 
 if __name__ == "__main__":
     main()
